39790e2e49707a30bdcf1a3a3032ec649f29b744
Chandru1511 <chandramohan.k@logituit.com>
2024-04-14 23:26:05 +0530

    forms added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import Form from "./Components/Form";
+import ColorList from "./Components/ColorList";
+import Values from "values.js";
+import { useState } from "react";
 
 function App() {
 
+const [colors, setColors]=useState(new Values('#f15025').all(10));
+  console.log(new Values('#f15025').all(10));
+  // new Values('#f15025') - This part creates a new instance of some kind of color-related object or class, possibly from a library or framework.
+  // .all(10) - This suggests that the all() method is being called on the color object created in the previous step, with 10 as an argument.
   return (
-    <>
-      <h2>Hello there</h2>
-    </>
-  )
+    <main>
+      <Form />
+      <ColorList />
+    </main>
+  );
 }
 
-export default App
+export default App;
